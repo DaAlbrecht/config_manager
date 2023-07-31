cfbb75f1c1f790129719d1b8b86c7652d1a0dbf2
DaAlbrecht <dadidi42@gmail.com>
2023-07-31 14:19:48 +0200

    feat: enable hidden files in pf, feat: enable open buff, feat: enable resizing tmux panes with arrow keys

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({ hidden = true })
+end, {})
+
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = '[P]ick [B]uffer' })
 
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
