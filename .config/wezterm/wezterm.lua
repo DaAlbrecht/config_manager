6eadae8f7c9fd7b12eae9e617940d2ee5cece677
DAA <42379074+DaAlbrecht@users.noreply.github.com>
2023-11-14 22:27:28 +0100

    feat: windows keybindings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -41,44 +41,86 @@ config.window_padding = {
 -- try at the top, could use the dead space of the notch
 
 --keybindings
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+    config.keys = {
+        -- Split Panes
+        { key = "d",          mods = "CMD",       action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+        {
+            key = "d",
+            mods = "CMD|SHIFT",
+            action = wezterm.action { SplitVertical = {
+                domain = "CurrentPaneDomain" } }
+        },
 
-config.keys = {
-    -- Split Panes
-    { key = "d",          mods = "CMD",       action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
-    {
-        key = "d",
-        mods = "CMD|SHIFT",
-        action = wezterm.action { SplitVertical = {
-            domain = "CurrentPaneDomain" } }
-    },
+        -- Pane Navigation
+        { key = "h",          mods = "CMD",       action = wezterm.action { ActivatePaneDirection = "Prev" } },
+        { key = "j",          mods = "CMD",       action = wezterm.action { ActivatePaneDirection = "Down" } },
+        { key = "k",          mods = "CMD",       action = wezterm.action { ActivatePaneDirection = "Up" } },
+        { key = "l",          mods = "CMD",       action = wezterm.action { ActivatePaneDirection = "Next" } },
 
-    -- Pane Navigation
-    { key = "h",          mods = "CMD",       action = wezterm.action { ActivatePaneDirection = "Prev" } },
-    { key = "j",          mods = "CMD",       action = wezterm.action { ActivatePaneDirection = "Down" } },
-    { key = "k",          mods = "CMD",       action = wezterm.action { ActivatePaneDirection = "Up" } },
-    { key = "l",          mods = "CMD",       action = wezterm.action { ActivatePaneDirection = "Next" } },
+        -- Resize Panes
+        { key = "h",          mods = "CMD|SHIFT", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+        { key = "j",          mods = "CMD|SHIFT", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
+        { key = "k",          mods = "CMD|SHIFT", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+        { key = "l",          mods = "CMD|SHIFT", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
 
-    -- Resize Panes
-    { key = "h",          mods = "CMD|SHIFT", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
-    { key = "j",          mods = "CMD|SHIFT", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
-    { key = "k",          mods = "CMD|SHIFT", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
-    { key = "l",          mods = "CMD|SHIFT", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
+        -- Close Pane
+        { key = "w",          mods = "CMD",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
 
-    -- Close Pane
-    { key = "w",          mods = "CMD",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+        -- Copy Mode
+        { key = "[",          mods = "CMD",       action = wezterm.action { CopyTo = "Clipboard" } },
 
-    -- Copy Mode
-    { key = "[",          mods = "CMD",       action = wezterm.action { CopyTo = "Clipboard" } },
+        -- Paste
+        { key = "]",          mods = "CMD",       action = wezterm.action { PasteFrom = "Clipboard" } },
 
-    -- Paste
-    { key = "]",          mods = "CMD",       action = wezterm.action { PasteFrom = "Clipboard" } },
+        --jump words
+        { key = "LeftArrow",  mods = "OPT",       action = wezterm.action { SendString = "\x1bb" } },
+        -- Make Option-Right equivalent to Alt-f; forward-word
+        { key = "RightArrow", mods = "OPT",       action = wezterm.action { SendString = "\x1bf" } },
 
-    --jump words
-    { key = "LeftArrow",  mods = "OPT",       action = wezterm.action { SendString = "\x1bb" } },
-    -- Make Option-Right equivalent to Alt-f; forward-word
-    { key = "RightArrow", mods = "OPT",       action = wezterm.action { SendString = "\x1bf" } },
+    }
+else
+    config.keys = {
+        -- Split Panes
+        { key = "d",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+        {
+            key = "d",
+            mods = "CMD|SHIFT",
+            action = wezterm.action { SplitVertical = {
+                domain = "CurrentPaneDomain" } }
+        },
 
-}
+        -- new tab 
+        { key = "t",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+
+        -- Pane Navigation
+        { key = "h",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { ActivatePaneDirection = "Prev" } },
+        { key = "j",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { ActivatePaneDirection = "Down" } },
+        { key = "k",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { ActivatePaneDirection = "Up" } },
+        { key = "l",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { ActivatePaneDirection = "Next" } },
+
+        -- Resize Panes
+        { key = "h",          mods = "ALT|SHIFT", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+        { key = "j",          mods = "ALT|SHIFT", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
+        { key = "k",          mods = "ALT|SHIFT", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+        { key = "l",          mods = "ALT|SHIFT", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
+
+        -- Close Pane
+        { key = "w",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+
+        -- Copy Mode
+        { key = "[",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { CopyTo = "Clipboard" } },
+
+        -- Paste
+        { key = "]",          mods = "ALT|SHIFT|CTRL",       action = wezterm.action { PasteFrom = "Clipboard" } },
+
+        --jump words
+        { key = "LeftArrow",  mods = "OPT",       action = wezterm.action { SendString = "\x1bb" } },
+        -- Make Option-Right equivalent to Alt-f; forward-word
+        { key = "RightArrow", mods = "OPT",       action = wezterm.action { SendString = "\x1bf" } },
+
+    }
+end
 
 -- and finally, return the configuration to wezterm
 return config
