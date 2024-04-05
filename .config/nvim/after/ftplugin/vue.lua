494c2d3907d37eecd4de8ef382d49c6dd0588f8f
DaAlbrecht <dadidi42@gmail.com>
2024-04-05 17:29:45 +0200

    feat: add vue support

---
+++ .config/nvim/after/ftplugin/vue.lua
@@ -0,0 +1,20 @@
+local lspconfig = require('lspconfig')
+local mason_registry = require('mason-registry')
+local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() ..
+    '/node_modules/@vue/language-server'
+
+lspconfig.tsserver.setup {
+    init_options = {
+        plugins = {
+            {
+                name = '@vue/typescript-plugin',
+                location = vue_language_server_path,
+                languages = { 'vue' },
+            },
+        },
+    },
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+}
+
+-- No need to set `hybridMode` to `true` as it's the default value
+lspconfig.volar.setup {}
