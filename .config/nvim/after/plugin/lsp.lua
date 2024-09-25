13a3eb35afffa97834e66bff039d6ee02586e5b2
DaAlbrecht <dadidi42@gmail.com>
2024-09-25 22:59:38 +0200

    start removing lspzero

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -45,25 +45,27 @@ lsp.setup_nvim_cmp({
     mapping = cmp_mappings
 })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-    vim.keymap.set("n", '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", '<leader>rn', vim.lsp.buf.rename, opts)
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", 'gr', require('telescope.builtin').lsp_references, opts)
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local opts = { buffer = args.buf, remap = false }
+        vim.keymap.set("n", '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", '<leader>rn', vim.lsp.buf.rename, opts)
 
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("i", "C-q", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", 'gr', require('telescope.builtin').lsp_references, opts)
 
-    vim.keymap.set("n", '<leader>ds', require('telescope.builtin').lsp_document_symbols, opts)
-    vim.keymap.set("n", '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, opts)
-end)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("i", "C-q", function() vim.lsp.buf.signature_help() end, opts)
 
+        vim.keymap.set("n", '<leader>ds', require('telescope.builtin').lsp_document_symbols, opts)
+        vim.keymap.set("n", '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, opts)
+    end,
+})
 
 require('lspconfig').typst_lsp.setup {
     settings = {
@@ -92,51 +94,4 @@ cmp.setup({
             menu = {},
         })
     }
-})
-
-
-Wso2lsp = function()
-    vim.lsp.start({
-        name = 'apache-synapse-lsp',
-        cmd = { "synapse-analyzer" },
-        root_dir = vim.loop.cwd(),
-        detached = false,
-        filetypes = { "foo" },
-        on_attach = function(client, bufnr)
-            print('apache-synapse-lsp server is running on buffer ' .. bufnr)
-        end,
-        autostart = true
-    })
-    vim.lsp.set_log_level 'debug'
-    if vim.fn.has 'nvim-0.5.1' == 1 then
-        require('vim.lsp.log').set_format_func(vim.inspect)
-    end
-end
-
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    pattern = "*apache.apachesynapse",
-    callback = function(_)
-        vim.b.filetype = "apachesynapse"
-        Wso2lsp()
-    end
-})
-
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-        vim.keymap.set("n", '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", '<leader>rn', vim.lsp.buf.rename, opts)
-
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", 'gr', require('telescope.builtin').lsp_references, opts)
-
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("i", "C-q", function() vim.lsp.buf.signature_help() end, opts)
-
-        vim.keymap.set("n", '<leader>ds', require('telescope.builtin').lsp_document_symbols, opts)
-        vim.keymap.set("n", '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, opts)
-    end,
 })
