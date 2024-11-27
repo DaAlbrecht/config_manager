1314129693dd198c851b274fbc6a2241bef639b0
DaAlbrecht <dadidi42@gmail.com>
2024-11-27 22:35:01 +0100

    lsp working

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -32,6 +32,7 @@ return {
           ['<Up>'] = cmp.mapping.scroll_docs(-4),
           ['<Down>'] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm { select = true },
           ['<C-l>'] = cmp.mapping(function()
             if luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -89,26 +89,29 @@ return {
                         },
                     },
                 },
-                bashls = true,
-                rust_analyzer = true,
-            }
-            local servers_to_install = vim.tbl_filter(function(key)
-                local t = servers[key]
-                if type(t) == "table" then
-                    return not t.manual_install
-                else
-                    return t
-                end
-            end, vim.tbl_keys(servers))
-
-            require("mason").setup()
-            local ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
+                bashls = {},
+                rust_analyzer = {},
             }
 
-            vim.list_extend(ensure_installed, servers_to_install)
-            require("mason-tool-installer").setup { ensure_installed = ensure_installed }
+            require('mason').setup()
+            local ensure_installed = vim.tbl_keys(servers or {})
+            vim.list_extend(ensure_installed, {
+                'stylua', -- Used to format Lua code
+            })
+
+            require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+            require('mason-lspconfig').setup {
+                handlers = {
+                    function(server_name)
+                        local server = servers[server_name] or {}
+                        -- This handles overriding only values explicitly passed
+                        -- by the server configuration above. Useful when disabling
+                        -- certain features of an LSP (for example, turning off formatting for ts_ls)
+                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                        require('lspconfig')[server_name].setup(server)
+                    end,
+                },
+            }
         end,
     }
 }
