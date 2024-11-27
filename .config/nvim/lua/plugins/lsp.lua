2704fca925efe82c2d80fe403f4f2362b4d17c7c
DaAlbrecht <dadidi42@gmail.com>
2024-11-27 01:01:20 +0100

    wip: switching to lazy

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,121 +1,114 @@
 return {
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
-  { 'Bilal2453/luvit-meta', lazy = true },
-  {
-    -- Main LSP Configuration
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       opts = {} },
-
-      -- Allows extra capabilities provided by nvim-cmp
-      'hrsh7th/cmp-nvim-lsp',
-    },
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(event)
-          -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-          -- to define small helper and utility functions so you don't have to repeat yourself.
-          --
-          -- In this case, we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
-          local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-
-          map('K', vim.lsp.buf.hover, 'Show [H]over')
-          map('gl', vim.diagnostic.open_float, 'Show [L]ine diagnostics')
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          map('[d', vim.diagnostic.goto_next, 'Go to next diagnostic')
-          map(']d', vim.diagnostic.goto_prev, 'Go to previous diagnostic')
-          map('C-q', vim.lsp.buf.signature_help, 'Show signature help', 'i')
-
-
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          -- The following code creates a keymap to toggle inlay hints in your
-          -- code, if the language server you are using supports them
-          --
-          -- This may be unwanted, since they displace some of your code
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
-        end,
-      })
-
-      if vim.g.have_nerd_font then
-        local icons = require("config.init").icons
-        local diagnostic_signs = {}
-        for type, icon in pairs(icons) do
-          diagnostic_signs[vim.diagnostic.severity[type]] = icon
-        end
-        vim.diagnostic.config { signs = { text = diagnostic_signs } }
-      end
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      local servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
+    {
+        -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+        -- used for completion, annotations and signatures of Neovim apis
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+            library = {
+                -- Load luvit types when the `vim.uv` word is found
+                { path = 'luvit-meta/library', words = { 'vim%.uv' } },
             },
-          },
         },
-      }
+    },
+    { 'Bilal2453/luvit-meta', lazy = true },
+    {
+        -- Main LSP Configuration
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            -- Automatically install LSPs and related tools to stdpath for Neovim
+            { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+            'williamboman/mason-lspconfig.nvim',
+            'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-      require('mason').setup()
+            -- Useful status updates for LSP.
+            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+            { 'j-hui/fidget.nvim',       opts = {} },
 
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
+            -- Allows extra capabilities provided by nvim-cmp
+            'hrsh7th/cmp-nvim-lsp',
         },
-      }
-    end,
-  }
+        config = function()
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(event)
+                    local map = function(keys, func, desc, mode)
+                        mode = mode or 'n'
+                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+                    end
+
+                    map('K', vim.lsp.buf.hover, 'Show [H]over')
+                    map('gl', vim.diagnostic.open_float, 'Show [L]ine diagnostics')
+                    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+                    map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+                    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+                    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                    map('[d', vim.diagnostic.goto_next, 'Go to next diagnostic')
+                    map(']d', vim.diagnostic.goto_prev, 'Go to previous diagnostic')
+                    map('C-q', vim.lsp.buf.signature_help, 'Show signature help', 'i')
+
+
+                    local client = vim.lsp.get_client_by_id(event.data.client_id)
+                    -- The following code creates a keymap to toggle inlay hints in your
+                    -- code, if the language server you are using supports them
+                    --
+                    -- This may be unwanted, since they displace some of your code
+                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+                        map('<leader>th', function()
+                            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+                        end, '[T]oggle Inlay [H]ints')
+                    end
+                end,
+            })
+
+            if vim.g.have_nerd_font then
+                local icons = require("config.init").icons
+                local diagnostic_signs = {}
+                for type, icon in pairs(icons) do
+                    diagnostic_signs[vim.diagnostic.severity[type]] = icon
+                end
+                vim.diagnostic.config { signs = { text = diagnostic_signs } }
+            end
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+            -- Enable the following language servers
+            --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+            local servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            completion = {
+                                callSnippet = 'Replace',
+                            },
+                        },
+                    },
+                },
+                bashls = true,
+                rust_analyzer = true,
+            }
+            local servers_to_install = vim.tbl_filter(function(key)
+                local t = servers[key]
+                if type(t) == "table" then
+                    return not t.manual_install
+                else
+                    return t
+                end
+            end, vim.tbl_keys(servers))
+
+            require("mason").setup()
+            local ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+            }
+
+            vim.list_extend(ensure_installed, servers_to_install)
+            require("mason-tool-installer").setup { ensure_installed = ensure_installed }
+        end,
+    }
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+-- Thanks to: https://github.com/tjdevries/config.nvim/tree/master
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
@@ -9,27 +10,27 @@ return {
         "nvim-telescope/telescope-smart-history.nvim",
         "nvim-telescope/telescope-ui-select.nvim",
         "kkharji/sqlite.lua",
+        "nvim-tree/nvim-web-devicons",
     },
     config = function()
         local data = assert(vim.fn.stdpath "data") --[[@as string]]
         require("telescope").setup {
-          defaults = {
-            file_ignore_patterns = { "dune.lock" },
-          },
-          extensions = {
-            wrap_results = true,
+            defaults = {
+                --file_ignore_patterns = { "dune.lock" },
+            },
+            extensions = {
+                wrap_results = true,
 
-            fzf = {},
-            history = {
-              path = vim.fs.joinpath(data, "telescope_history.sqlite3"),
-              limit = 100,
+                fzf = {},
+                history = {
+                    path = vim.fs.joinpath(data, "telescope_history.sqlite3"),
+                    limit = 100,
+                },
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown {},
+                },
             },
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown {},
-            },
-          },
         }
-        
         pcall(require("telescope").load_extension, "fzf")
         pcall(require("telescope").load_extension, "smart_history")
         pcall(require("telescope").load_extension, "ui-select")
