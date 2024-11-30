6e606b0a199415fc9f15dd3437bed2485500a55a
DaAlbrecht <dadidi42@gmail.com>
2024-11-30 11:19:43 +0100

    feat: add ignore files to oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,25 @@
+-- https://github.com/tjdevries/config.nvim/blob/master/lua/custom/plugins/oil.lua
+CustomOilBar = function()
+    local path = vim.fn.expand "%"
+    path = path:gsub("oil://", "")
+
+    return "  " .. vim.fn.fnamemodify(path, ":.")
+end
+
 return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {
+        win_options = {
+            winbar = "%{v:lua.CustomOilBar()}",
+        },
         view_options = {
-            -- Show files and directories that start with "."
-            show_hidden = true
+            show_hidden = true,
+            is_always_hidden = function(name, _)
+                local folder_skip = { ".DS_Store" }
+                return vim.tbl_contains(folder_skip, name)
+            end,
         },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
