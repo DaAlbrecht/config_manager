a41c511ba465701e27e23ee6ff761046193fd061
DaAlbrecht <dadidi42@gmail.com>
2025-08-08 17:01:08 +0200

    feat: add swww and randomize wallpaper on startup

---
+++ .config/hypr/scripts/wallpapers.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+WALLPAPER_DIR="$HOME/wallpapers/"
+CURRENT=$(swww query | head -n1 | grep -oP 'image: \K\S+')
+WALLPAPER=$(fd -E "$(basename "$CURRENT")" . "$WALLPAPER_DIR" | shuf -n 1)
+swww img "$WALLPAPER"
