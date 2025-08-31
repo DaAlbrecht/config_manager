a25551d78db590043b48ef784e95ef45473b916f
DaAlbrecht <dadidi42@gmail.com>
2025-08-31 21:47:12 +0200

    feat: change wallpaper transitioning speed

--- .config/hypr/scripts/wallpapers.sh
+++ .config/hypr/scripts/wallpapers.sh
@@ -3,4 +3,4 @@
 WALLPAPER_DIR="$HOME/wallpapers/"
 CURRENT=$(swww query | head -n1 | grep -oP 'image: \K\S+')
 WALLPAPER=$(fd -E "$(basename "$CURRENT")" . "$WALLPAPER_DIR" | shuf -n 1)
-swww img "$WALLPAPER"
+swww img "$WALLPAPER" --transition-fps 144 --transition-duration 1
